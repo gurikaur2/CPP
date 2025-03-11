2010d8839cc24608bc7f3acd6400500b775bae5b
gurikaur2 <gurnoorkaur.mail@gmail.com>
2025-03-11 22:10:13 +0530

    Initial commit

---
+++ 05_loops/for.cpp
@@ -0,0 +1,13 @@
+/* Write a program that prints the brewing instructions for making 5 cups of tea. 
+The brewing process should be printed once for each cup using a for loop.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    
+    return 0;
+}
